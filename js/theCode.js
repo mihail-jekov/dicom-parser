91a6504b955f7f0f1efb06daa47ee7b4ec5745a5
Mihail Jekov <mihail.jekov@gmail.com>
2014-11-24 17:51:10 +0200

    Some improvments

--- js/theCode.js
+++ js/theCode.js
@@ -9,19 +9,25 @@ $(function ()
 	canvas.fillText("Drop Some Dicom File :)", 50, 250);
 });
 
+var dicomDocument = {};
+
 var dicomStudy = {};
 var dicomImage = {};
 var dicomPatient = {};
 var dicomOverlay = {};
+var dicomIcon = {};
+var parseIcon = false;
+
+var currentImage = {};
 
 var parseTime;
 
-function publishData()
+function publishData(image)
 {
 	var result = "Image:<br //><br //>";
-	for (var property in dicomImage)
+	for (var property in image)
 	{
-		result += property + ": " + dicomImage[property] + "<br //>";
+		result += property + ": " + image[property] + "<br //>";
 	}
 	result += "<br //><br //>Study data:<br //><br //>";
 	for (var property in dicomStudy)
@@ -49,20 +55,23 @@ function parseImageData(byte16Array, length, byte8Array)
 	{
 		switch ((byte16Array[i] << 16) + byte16Array[i + 1])
 		{
-			case 0x00280010: 	dicomImage.width = byte16Array[i + 4]; c++; i += 4; break;
-			case 0x00280011: 	dicomImage.height = byte16Array[i + 4]; i += 4; break;
-			case 0x00280002: 	dicomImage.spp = byte16Array[i + 4]; i += 4; break;
-			case 0x00280004:	dicomImage.photometricInterpretation = parseCS(byte16Array[i + 3], byte8Array, (i + 4) * 2); break;
-			case 0x00280008:	dicomImage.numberOfFrames = parseCS(byte16Array[i + 3], byte8Array, (i + 4) * 2); break;
-			case 0x00280100:	dicomImage.bitsAllocated = byte16Array[i + 4]; i += 4; break;
-			case 0x00280101:	dicomImage.bitsStored = byte16Array[i + 4]; i += 4; break;
-			case 0x00280102:	dicomImage.highBit = byte16Array[i + 4]; i += 4; break;
-			case 0x00282124:	dicomImage.lossyImageCompressionMethod = parseCS(byte16Array[i + 3], byte8Array, (i + 4) * 2); break;
+			case 0x000051B0:	currentImage.overlays = byte16Array[i + 4]; i += 4; break;
+			
+			case 0x00280010: 	currentImage.width = byte16Array[i + 4]; i += 4; break;
+			case 0x00280011: 	currentImage.height = byte16Array[i + 4]; i += 4; break;
+			case 0x00280002: 	currentImage.spp = byte16Array[i + 4]; i += 4; break;
+			case 0x00280004:	currentImage.photometricInterpretation = parseCS(byte16Array[i + 3], byte8Array, (i + 4) * 2); break;
+			case 0x00280006:	currentImage.planarConfiguration = byte16Array[i + 4]; i += 4; break;
+			case 0x00280008:	currentImage.numberOfFrames = parseCS(byte16Array[i + 3], byte8Array, (i + 4) * 2); break;
+			case 0x00280100:	currentImage.bitsAllocated = byte16Array[i + 4]; i += 4; break;
+			case 0x00280101:	currentImage.bitsStored = byte16Array[i + 4]; i += 4; break;
+			case 0x00280102:	currentImage.highBit = byte16Array[i + 4]; i += 4; break;
+			case 0x00282124:	currentImage.lossyImageCompressionMethod = parseCS(byte16Array[i + 3], byte8Array, (i + 4) * 2); break;
 			case 0x00281100:	var a = 10; break;
-			case 0x00282110:	dicomImage.lossyImageCompression = parseCS(byte16Array[i + 3], byte8Array, (i + 4) * 2); break;
+			case 0x00282110:	currentImage.lossyImageCompression = parseCS(byte16Array[i + 3], byte8Array, (i + 4) * 2); break;
 			
 			//Тук не трябва да чете CS, а UI
-			case 0x00020010:	dicomImage.transferSyntaxUniqueIdentification = parseCS(byte16Array[i + 3], byte8Array, (i + 4) * 2); break;
+			case 0x00020010:	currentImage.transferSyntaxUniqueIdentification = parseCS(byte16Array[i + 3], byte8Array, (i + 4) * 2); break;
 			
 			case 0x00100010:	dicomPatient.name = parsePN(byte16Array[i + 3], byte8Array, (i + 4) * 2); i += 4; break;
 			//Същото важи и за тук, не е CS ами LO (long string)
@@ -88,40 +97,89 @@ function parseImageData(byte16Array, length, byte8Array)
 			case 0x60001202:	dicomOverlay.overlayGreen = byte16Array[i + 4]; i += 4; break;
 			case 0x60001203:	dicomOverlay.overlayBlue = byte16Array[i + 4]; i += 4; break;
 			
+/*			case 0x60020010:	alert(1); break;
+			case 0x60040010:	alert(1); break;
+			case 0x60060010:	alert(1); break;
+			case 0x60080010:	alert(1); break;
+			case 0x600A0010:	alert(1); break;
+			case 0x600C0010:	alert(1); break;
+			case 0x600E0010:	alert(1); break;
+			case 0x60100010:	alert(1); break;
+			case 0x60120010:	alert(1); break;
+			case 0x60140010:	alert(1); break;
+			case 0x60160010:	alert(1); break;
+			case 0x60180010:	alert(1); break;
+			case 0x601A0010:	alert(1); break;
+			case 0x601C0010:	alert(1); break;
+			case 0x601E0010:	alert(1); break;*/
 			
 			
-			case 0x7FE00000:	dicomImage.pixelDataGroupLength = (byte16Array[i + 4] << 16) + byte16Array[i + 5]; i += 5;
-			case 0x7FE00010:	publishData();
-								parseTime = new Date().getTime() - parseTime;
-					//			i = (i + 6) * 2; 	//for 8 bits array mode
-								i += 6;				//for 16 bits array mode
-								var canvas = document.getElementById("canvas").getContext("2d");
-								var canvasData = canvas.createImageData(dicomImage.width, dicomImage.height);
-								var d = canvasData.data;
-								var startTime = new Date().getTime();
-								
-								if (dicomImage.bitsStored > 14)
+			
+			case 0x00880200:	dicomImage = currentImage;
+								currentImage = dicomIcon;
+								parseIcon = true;
+								break;
+			case 0x7FE00000:	currentImage.pixelDataGroupLength = (byte16Array[i + 4] << 16) + byte16Array[i + 5]; i += 5; break;
+			case 0x7FE00010:	currentImage.pixelData = byte8Array.subarray((i + 6) * 2, (i + 6) * 2 + (currentImage.width * currentImage.height) * 3);
+								if(parseIcon)
 								{
-									for (var pos = 0; pos < dicomImage.width * dicomImage.height; pos++, i += 1) // += 1 for 16 bits, += 2 for 8
-									{
-										d[pos * 4] = byte16Array[i] / 2; d[pos * 4 + 1] = byte16Array[i] / 2; d[pos * 4 + 2] = byte16Array[i] / 2; d[pos * 4 + 3] = 255;
-									}
+									dicomIcon = currentImage;
+									currentImage = dicomImage;
+									parseIcon = false;
 								}
-								else
-								{
-									for (var pos = 0; pos < dicomImage.width * dicomImage.height; pos++, i += 1) // += 1 for 16 bits, += 2 for 8
-									{
-										d[pos * 4] = byte16Array[i] >> 6; d[pos * 4 + 1] = byte16Array[i] >> 6; d[pos * 4 + 2] = byte16Array[i] >> 6; d[pos * 4 + 3] = 255;
-									}
-								}
-								
-								canvas.clearRect(0, 0, 800, 500);
-								canvas.putImageData(canvasData, 0, 0);
-								
-								$("#status").text("Parse time: " + parseTime + "ms, Image draw time: " + (new Date().getTime() - startTime) + "ms");
+								break;
 		}
 		i++;
 	}
+}
+
+function drawImage(image)
+{
+	publishData(image);
+	parseTime = new Date().getTime() - parseTime;
+	//			i = (i + 6) * 2; 	//for 8 bits array mode
+	var canvas = document.getElementById("canvas").getContext("2d");
+	var canvasData = canvas.createImageData(image.height, image.width);
+	var d = canvasData.data;
+	var startTime = new Date().getTime();
+	if (image.photometricInterpretation.toLowerCase() === 'RGB '.toLowerCase())
+	{
+		if (image.planarConfiguration == 0)
+		{
+			for (var pos = 0; pos < image.width * image.height; pos++) // += 1 for 16 bits, += 2 for 8
+			{
+				d[pos * 4] = image.pixelData[pos * 3]; d[pos * 4 + 1] = image.pixelData[pos * 3 + 1]; d[pos * 4 + 2] = image.pixelData[pos * 3 + 2]; d[pos * 4 + 3] = 255;
+			}
+		}
+		else if (image.planarConfiguration == 1)
+		{
+			var size = image.width * image.height;
+			for (var pos = 0; pos < size; pos++) // += 1 for 16 bits, += 2 for 8
+			{
+				d[pos * 4] = image.pixelData[pos]; d[pos * 4 + 1] = image.pixelData[pos + size]; d[pos * 4 + 2] = image.pixelData[pos + size * 2]; d[pos * 4 + 3] = 255;
+			}
+		}
+	}
+	else if (currentImage.bitsStored > 14)
+	{
+		for (var pos = 0; pos < image.width * image.height; pos++) // += 1 for 16 bits, += 2 for 8
+		{
+			d[pos * 4] = image.pixelData[pos] / 2; d[pos * 4 + 1] = image.pixelData[pos] / 2; d[pos * 4 + 2] = image.pixelData[pos] / 2; d[pos * 4 + 3] = 255;
+		}
+	}
+	else
+	{
+		for (var pos = 0; pos < image.width * image.height; pos++) // += 1 for 16 bits, += 2 for 8
+		{
+			d[pos * 4] = image.pixelData[pos] / 2; d[pos * 4 + 1] = image.pixelData[pos] / 2; d[pos * 4 + 2] = image.pixelData[pos] / 2; d[pos * 4 + 3] = 255;
+		}
+	}
+								
+	canvas.clearRect(0, 0, 800, 500);
+	canvas.putImageData(canvasData, 0, 0);
+								
+	$("#status").text("Parse time: " + parseTime + "ms, Image draw time: " + (new Date().getTime() - startTime) + "ms");
+	return;
 }
 
 function readFile(file)
@@ -133,6 +191,7 @@ function readFile(file)
         var byte16Array = new Uint16Array(arrayBuffer);
 		var byte8Array = new Uint8Array(arrayBuffer);
 		parseImageData(byte16Array, byte16Array.length, byte8Array);
+		drawImage(dicomImage);
 		
         var kb = byte16Array.length / 1024;
         var mb = kb / 1024;
